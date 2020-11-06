8af4968d1d5ab7a2b3408ccb240af4f9b67466d1
Sakura Blossom <sakurablossom@Sakuras-MacBook-Pro.local>
2020-11-06 08:57:33 -0800

    I am happy

---
+++ script/main.js
@@ -0,0 +1,252 @@
+
+let PARAMETERS = {
+  borderColor: "#BE4BDB",
+  playedColor: "#000",
+  backgroundColor: "rgba(142,187,196,.2)",
+  iconColor: "#fff",
+  borderWidth: 2,
+  size: 48,
+  className: 'circle-audio-player'
+};
+let pi = Math.PI;
+let doublePi = pi * 2;
+let circumference = -pi / 2;
+let animTime = 200;
+let loaderTime = 1800;
+
+let audioPlayer = function(choices) {
+  choices = choices || {};
+  for (let property in PARAMETERS) {
+    this[property] = choices[property] || PARAMETERS[property];
+  }
+ console.log(this);
+  this.canvas = document.createElement('canvas');
+  this.canvas.setAttribute('class', `${this.className} is-loading`);
+  this.canvas.addEventListener('mousedown', (function () {
+      if (this.playing) {
+          this.pause();
+      }
+      else {
+          this.play();
+      }
+  }).bind(this));
+  this.context = this.canvas.getContext('2d');
+
+  this.setAudio(choices.audio);
+  this.setSize(this.size);
+
+  (function animationLoop (now) {
+      // check if we need to update anything
+      if (this.animating) {
+          this.updateAnimations(now);
+      }
+      if (this._forceDraw || this.playing || this.animating || this.loading) {
+          this.drawing();
+          this._forceDraw = false;
+      }
+
+      requestAnimationFrame(animationLoop.bind(this));
+  }).call(this, new Date().getTime());
+};
+audioPlayer.prototype = {
+  animateIcon: function (to, from) {
+      this._animationProps = {
+          animStart: null,
+          from: from,
+          to: to
+      };
+      if (from) {
+          this.animating = true;
+      }
+      else {
+          this._animationProps.current = this._icons[to].slice();
+          this.draw();
+      }
+  },
+  updateAnimations: function (now) {
+      this._animationProps.animStart = this._animationProps.animStart || now;
+      let deltaTime = now - this._animationProps.animStart;
+      let perc = (1 - Math.cos(deltaTime / animTime * pi / 2));
+      if (deltaTime >= animTime) {
+          this.animating = false;
+          perc = 1;
+          this._animationProps.current = this._icons[this._animationProps.to].slice();
+          this.draw();
+      }
+      else {
+          let from = this._icons[this._animationProps.from];
+          let current = [];
+          for (let i = 0; i < from.length; i++) {
+              current.push([]);
+              for (let j = 0; j < from[i].length; j++) {
+                  current[i].push([]);
+                  let to = this._icons[this._animationProps.to][i][j];
+                  current[i][j][0] = from[i][j][0] + (to[0] - from[i][j][0]) * perc;
+                  current[i][j][1] = from[i][j][1] + (to[1] - from[i][j][1]) * perc;
+              }
+          }
+          this._animationProps.current = current;
+      }
+  },
+  drawing: function (progress) {
+      if (isNaN(progress)) {
+          progress = this.audio.currentTime / this.audio.duration || 0;
+      }
+
+      this.context.clearRect(0, 0, this.size, this.size);
+
+      this.context.beginPath();
+      this.context.arc(this.halfSize, this.halfSize, this.halfSize - (this.borderWidth / 2), 0, doublePi);
+      this.context.closePath();
+      this.context.fillStyle = this.backgroundColor;
+      this.context.fill();
+
+      this.context.lineWidth = this.borderWidth;
+      this.context.strokeStyle = this.borderColor;
+      this.context.stroke();
+
+      if (progress > 0) {
+          this.context.beginPath();
+          this.context.arc(this.halfSize, this.halfSize, this.halfSize - (this.borderWidth / 2), circumference, circumference + doublePi * progress);
+          this.context.strokeStyle = this.playedColor;
+          this.context.stroke();
+      }
+
+      this.context.fillStyle = this.iconColor;
+      if (this.loading) {
+          let loaderOffset = -Math.cos((new Date().getTime() % (loaderTime)) / (loaderTime) * pi) * doublePi - (pi / 3) - (pi / 2);
+          this.context.beginPath();
+          this.context.arc(this.halfSize, this.halfSize, this.halfSize / 3, loaderOffset, loaderOffset + pi / 3 * 2);
+          this.context.strokeStyle = this.iconColor;
+          this.context.stroke();
+      }
+      else {
+          this.context.beginPath();
+          let icon = (this.animationProps && this.animationProps.current) || this._icons.play;
+          for (let i = 0; i < icon.length; i++) {
+              this.context.moveTo(icon[i][0][0], icon[i][0][1]);
+
+              for (let k = 1; k < icon[i].length; k++) {
+                  this.context.lineTo(icon[i][k][0], icon[i][k][1]);
+              }
+          }
+
+          this.context.fill();
+          this.context.strokeStyle = this.iconColor;
+          this.context.lineWidth = 2;
+          this.context.linejoin = 'miter';
+          this.context.stroke();
+      }
+  },
+  setState: function (state) {
+      this.playing = false;
+      this.loading = false;
+      if (state === 'playing') {
+          this.playing = true;
+          this.animateIcon('pause', 'play');
+      }
+      else if (state === 'loading') {
+          this.loading = true;
+      }
+      else if (this.state !== 'loading') {
+          this.animateIcon('play', 'pause');
+      }
+      else {
+          this.animateIcon('play', null);
+      }
+      this.state = state;
+      this.canvas.setAttribute('class', this.className + ' is-' + state);
+      this.draw();
+  },
+  
+  draw: function () {
+      this._forceDraw = true;
+  },
+  setSize: function (size) {
+      this.size = size;
+      this.halfSize = size / 2; 
+      this.canvas.width = size;
+      this.canvas.height = size;
+
+      let iconSize = this.size / 2;
+      let pauseGap = iconSize / 10;
+      let playLeft = Math.cos(pi / 3 * 2) * (iconSize / 2) + this.halfSize;
+      let playRight = iconSize / 2 + this.halfSize;
+      let playHalf = (playRight - playLeft) / 2 + playLeft;
+      let top = this.halfSize - Math.sin(pi / 3 * 2) * (iconSize / 2);
+      let bottom = this.size - top;
+      let pauseLeft = this.halfSize - iconSize / 3;
+      let pauseRight = this.size - pauseLeft;
+      this._icons = {
+          play: [
+              [
+                  [playLeft, top],
+                  [playHalf, (this.halfSize - top) / 2 + top],
+                  [playHalf, (this.halfSize - top) / 2 + this.halfSize],
+                  [playLeft, bottom]
+              ],
+              [
+                  [playHalf, (this.halfSize - top) / 2 + top],
+                  [playRight, this.halfSize],
+                  [playRight, this.halfSize],
+                  [playHalf, (this.halfSize - top) / 2 + this.halfSize]
+              ]
+          ],
+          pause: [
+              [
+                  [pauseLeft, top + pauseGap],
+                  [this.halfSize - pauseGap, top + pauseGap],
+                  [this.halfSize - pauseGap, bottom - pauseGap],
+                  [pauseLeft, bottom - pauseGap]
+              ],
+              [
+                  [this.halfSize + pauseGap, top + pauseGap],
+                  [pauseRight, top + pauseGap],
+                  [pauseRight, bottom - pauseGap],
+                  [this.halfSize + pauseGap, bottom - pauseGap]
+              ]
+          ]
+      };
+
+      if (this.animationProps && this.animationProps.current) {
+          this.animateIcon(this.animationProps.to);
+      }
+      if (!this.playing) {
+          this.draw();
+      }
+  },
+  setAudio: function (audioUrl) {
+      this.audio = new Audio(audioUrl);
+      this.setState('loading');
+
+      this.audio.addEventListener('canplaythrough', (function () {
+          this.setState('paused');
+      }).bind(this));
+      this.audio.addEventListener('play', (function () {
+          this.setState('playing');
+      }).bind(this));
+      this.audio.addEventListener('pause', (function () {
+          
+          if (this.audio.currentTime === this.audio.duration) {
+              this.audio.currentTime = 0;
+          }
+          this.setState('paused');
+      }).bind(this));
+  },
+  appendTo: function (element) {
+      element.appendChild(this.canvas);
+  },
+  play: function () {
+      this.audio.play();
+  },
+  pause: function () {
+      this.audio.pause();
+  }
+};
+
+let cap = new audioPlayer({
+  audio: 'audio/31YuYen.mp3',
+size: 20,
+borderWidth: 8
+});
+cap.appendTo(playerContent);
---
+++ script/toph.js
@@ -0,0 +1,55 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const theBoulders = document.querySelectorAll('.the-boulder');
+const btn = document.querySelector('.btn');
+const msg1 = document.querySelector('.msg1');
+const msg2 = document.querySelector('.msg2');
+
+
+let lastHole;
+let timeUp = false;
+let score = 0;
+let currentTime = timeUp.textContent;
+
+function randomHole(holes) {
+  const randomHole = Math.floor(Math.random() * holes.length);
+  const hole = holes[randomHole];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  
+  lastHole = hole; 
+  return hole;
+}
+
+function popUp() {
+  const time = Math.random() * (1300 - 400) + 400;
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(function () {
+      hole.classList.remove('up'); 
+      if (!timeUp){
+        popUp();
+      } else {
+        return document.querySelector('msg1').innerHTML = 'You have brought great shame to the Toph name!';
+      }
+    }, time);
+}
+
+btn.addEventListener('click', function() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  popUp();
+  setTimeout(() => timeUp = true, 20000)
+});
+
+function bonk(e) {
+  if(!e.isTrusted) return;
+  score++;
+  this.parentNode.classList.remove('up');
+  scoreBoard.textContent = score;
+  console.log(this);
+}
+
+theBoulders.forEach(theBoulder => theBoulder.addEventListener('click', bonk));
