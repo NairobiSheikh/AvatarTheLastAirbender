649cd4ce37fe42cce338f37d66e75465da065631
Sakura Blossom <sakurablossom@Sakuras-MacBook-Pro.local>
2020-11-07 17:27:11 -0800

    Fixed Toph's js file

--- script/toph.js
+++ script/toph.js
@@ -1,58 +1,74 @@
+/*
+ * Getting the DOM Elements
+ */
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const theBoulders = document.querySelectorAll('.the-boulder');
 const btn = document.querySelector('.btn');
 const countdownBoard = document.querySelector('.countdown');
 
+/*
+ * setting the up the random length of time
+ */
 let lastHole;
-let timeUp = false;
-let timeLimit = 20000;
+const timeLimit = 20000;
 let score = 0;
-let countdown;
+let countdown = timeLimit/1000;
 
-function randomHole(holes) {
-  const randomHole = Math.floor(Math.random() * holes.length);
-  const hole = holes[randomHole];
-  if (hole === lastHole) {
-    return randomHole(holes);
+/**
+ * retuns a random hole that is not the same as last hole.
+ */
+function randomHole() {
+  while (true) {
+    const randomHole = Math.floor(Math.random() * holes.length);
+    const hole = holes[randomHole];
+    if (hole !== lastHole) {
+      lastHole = hole;
+      return hole;
+    }
   }
-  lastHole = hole; 
-  return hole;
 }
 
+/**
+ * brings up the theBoulders in random holes and when time runs out it hides the theBoulders.
+ */
 function popUp() {
-  const time = Math.random() * (1300 - 400) + 400;
-  const hole = randomHole(holes);
+  const time = Math.random() * 1300;
+  const hole = randomHole();
   hole.classList.add('up');
   setTimeout(function () {
-      hole.classList.remove('up'); 
-      if (!timeUp)
-        popUp();
-    }, time);
+      hole.classList.remove('up');
+  }, time);
 }
 
+/**
+ * start and ends the game
+ */
 btn.addEventListener('click', function() {
-  countdown = timeLimit/1000;
   scoreBoard.textContent = 0;
   countdownBoard.textContent = countdown,
-  timeUp = false;
   score = 0;
-  popUp();
-  setTimeout(function(){
-    timeUp = true;
-  }, timeLimit);
-
   let startCountdown = setInterval(function(){
     countdown -= 1;
-    countdownBoard.textContent = countdown;
-    if (countdown < 0) {
+    if (countdown < 1) {
       countdown = 0;
       clearInterval(startCountdown);
-      countdownBoard.textContent = 'Times up!! Thank you for participating on our planet! Toph rules!!'
+      countdownBoard.textContent = 'Times up!! You have brought great shame to Toph\'s name!!'
+    } else if (score >= 5) {
+      clearInterval(startCountdown);
+      countdownBoard.textContent = 'You have brought great pride to Toph\'s name.';
+    } else {
+      countdownBoard.textContent = countdown;
+      popUp();
     }
   }, 1000);
+
 });
 
+/**
+ * event that fires when theBoulder clicked
+ * @param {object} e
+ */
 function bonk(e) {
   if(!e.isTrusted) return;
   score++;
@@ -60,4 +76,7 @@ function bonk(e) {
   scoreBoard.textContent = score;
 }
 
+/**
+ * fires the bonke() function
+ */
 theBoulders.forEach(theBoulder => theBoulder.addEventListener('click', bonk));
